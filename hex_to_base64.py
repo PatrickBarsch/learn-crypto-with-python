5e1280c6530e4166a93165595f257ace5dbec08e
Patrick <PatrickGB@gmx.de>
2022-06-30 21:32:34 +0200

    Why do we even have base64?

--- hex_to_base64.py
+++ hex_to_base64.py
@@ -13,8 +13,8 @@ def xor(hex_1: str, hex_2: str) -> hex:
     return int(hex_1, 16) ^ int(hex_2, 16)
 
 
-def xor_bytes_against_single_char(our_bytes: bytes, single_char: str) -> bytearray:
-    return bytearray([(one_byte ^ ord(single_char)) for one_byte in our_bytes])
+def xor_bytes_against_single_char(our_bytes: bytes, other: chr) -> bytearray:
+    return bytearray([(one_byte ^ ord(other)) for one_byte in our_bytes])
 
 
 if __name__ == '__main__':
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     s22 = '686974207468652062756c6c277320657965'
     print("{:x}".format(xor(s21, s22)))
     s3 = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-    for i in range(0,255):
-        print_as_base64(xor_bytes_against_single_char(hex_to_bytes(s3), chr(i)))
+    for i in range(0, 255):
+        print(xor_bytes_against_single_char(hex_to_bytes(s3), chr(i)))
