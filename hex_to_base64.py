661238ad18919e2ce9838ea487b4a5aa6cc9ee14
Patrick <PatrickGB@gmx.de>
2022-07-28 18:55:50 +0200

    Checking for a single word gives the correct solution, albeit with to much output

--- hex_to_base64.py
+++ hex_to_base64.py
@@ -1,4 +1,5 @@
 import base64
+import enchant
 
 
 def hex_to_bytes(input_hex: str) -> bytes:
@@ -13,8 +14,25 @@ def xor(hex_1: str, hex_2: str) -> hex:
     return int(hex_1, 16) ^ int(hex_2, 16)
 
 
-def xor_bytes_against_single_char(our_bytes: bytes, other: chr) -> bytearray:
+def xor_bytes_to_char(our_bytes: bytes, other: chr) -> bytearray:
     return bytearray([(one_byte ^ ord(other)) for one_byte in our_bytes])
+
+
+def xor_against_all_chars(encrypted: bytes) -> list[str]:
+    return [(xor_bytes_to_char(encrypted, chr(i))).decode('ascii') for i in range(0, 127)]
+
+
+def check_for_words(candidates: list[str]):
+    return [c for c in candidates if has_word(c)]
+
+
+def has_word(chars) -> bool:
+    d = enchant.Dict("en_US")
+    split_to_words = chars.split(" ")
+    try:
+        return any((d.check(seq) for seq in split_to_words))
+    except:
+        return False
 
 
 if __name__ == '__main__':
@@ -24,5 +42,6 @@ if __name__ == '__main__':
     s22 = '686974207468652062756c6c277320657965'
     print("{:x}".format(xor(s21, s22)))
     s3 = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-    for i in range(0, 255):
-        print(xor_bytes_against_single_char(hex_to_bytes(s3), chr(i)))
+    possible_solutions = xor_against_all_chars(hex_to_bytes(s3))
+    for solution in check_for_words(possible_solutions):
+        print(solution)
